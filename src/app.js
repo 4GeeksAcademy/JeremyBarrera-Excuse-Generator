4d1353c0581349c098f2d1866d65a520d04fa2fd
JeremyBarrera <112196500+JeremyBarrera@users.noreply.github.com>
2025-01-22 22:22:30 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
 import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function getrandom(num) {
+    return Math.floor(Math.random() * num);
+  }
+
+  function excuse() {
+    return (
+      who[getrandom(who.length)] +
+      " " +
+      action[getrandom(action.length)] +
+      " " +
+      what[getrandom(what.length)] +
+      " " +
+      when[getrandom(when.length)]
+    );
+  }
+
+  document.getElementById("excuse").innerText = excuse();
 };
